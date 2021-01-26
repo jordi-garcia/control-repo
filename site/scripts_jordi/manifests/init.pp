f8b635a044d287b373feebef46d3630d68dc020d
jordi-garcia <jordi.garcia@puppet.com>
2021-01-26 20:32:30 +0000

    Update init.pp
    Completed addtohosts code

--- site/scripts_jordi/manifests/init.pp
+++ site/scripts_jordi/manifests/init.pp
@@ -5,24 +5,24 @@ class scripts_jordi {
   # Trying to get public IP address using Puppet facts
   # using different syntax for practice
 
-  $startextip = "10.234"
-  $searchip = "\/\^$startextip\/"
+  $startextip = '10.234'
+  $searchip = "/^$startextip/"
 
-  if $facts['ec2_metadata']['public-ipv4'] =~ $searchip  {
+  if $facts['ec2_metadata']['public-ipv4'] =~ Regexp($startextip) {
 
     # requesting external IP and fqdn 
     $extip = $facts['ec2_metadata']['public-ipv4']
-    $fqdn = $facts['fqdn']
+    $pe_fqdn = $facts['fqdn']
 
     # building the message_addtohosts variable
     # it should resemble: sed -i "2i10.234.3.2 pe-201984-master.puppetdebug.vlan /etc/hosts"
 
-    $message_addtohosts = "sed -i '2i$extip $fqdn'"
+    $message_addtohosts = "sed -i '2i$extip $pe_fqdn'"
 
   }
   else {
 
-    $message_addtohosts  = "Didn't get and IP starting with: $searchip  $startextip"
+    $message_addtohosts  = "Didn't get and IP starting with: $startextip"
 
   }
 
