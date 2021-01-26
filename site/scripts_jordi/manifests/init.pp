26a8c92625944e49ce135df9f230471723cb6208
jordi-garcia <jordi.garcia@puppet.com>
2021-01-26 18:14:03 +0000

    entering IP in file

--- site/scripts_jordi/manifests/init.pp
+++ site/scripts_jordi/manifests/init.pp
@@ -2,10 +2,14 @@
 # for mainly bolt commands
 
 class scripts_jordi {
-  # Equivalent resources:
+  # Trying to get public IP address
+
+  if $facts['ec2_metadata']['public-ipv4'] =~ /^10.234/ {
+    $extip = $facts['ec2_metadata']['public-ipv4']
+  }
 
   file { '/opt/scripts/newaddtohosts.sh':
     ensure  => 'present',
-    content => 'sed -i "2i10.234.3.2 pe-201984-master.puppetdebug.vlan" /etc/hosts',
+    content => 'sed -i "2i10.234.3.2 pe-201984-master.puppetdebug.vlan /etc/hosts $extip"',
   }
 }
