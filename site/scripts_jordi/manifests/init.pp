9bccc61ad90baf7dba7712d207e7b2ab6e2a4c0c
jordi-garcia <jordi.garcia@puppet.com>
2021-01-26 17:00:37 +0000

    Update init.pp

--- site/scripts_jordi/manifests/init.pp
+++ site/scripts_jordi/manifests/init.pp
@@ -5,6 +5,7 @@ class scripts_jordi {
   # Equivalent resources:
 
   file { '/opt/scripts/newaddtohosts.sh':
-    ensure => '/opt/scripts/newaddtohosts.sh',
-  } 
+    ensure  =>  link,
+    target  =>  '/opt/scripts/newaddtohosts.sh',
+  }
 }
