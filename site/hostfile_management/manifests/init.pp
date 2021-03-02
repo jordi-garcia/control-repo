6ba50f2494617f80ef685aedfee688fd3beae3cf
jordi-garcia <jordi.garcia@puppet.com>
2021-03-02 15:38:51 +0000

    added removal of the hostfile initially

--- site/hostfile_management/manifests/init.pp
+++ site/hostfile_management/manifests/init.pp
@@ -6,25 +6,32 @@
 #   include hostfile_management
 class hostfile_management {
 
+  file { '/etc/hosts':
+    ensure  => 'absent',
+  }
+
   host { 'localhost':
-  ensure       => 'present',
-  host_aliases => ['localhost.localdomain', 'localhost4', 'localhost4.localdomain4'],
-  ip           => '127.0.0.1',
-  target       => '/etc/hosts',
+    ensure       => 'present',
+    host_aliases => ['localhost.localdomain', 'localhost4', 'localhost4.localdomain4'],
+    ip           => '127.0.0.1',
+    target       => '/etc/hosts',
+    subscribe    => File['/etc/hosts'],
   }
 
   host { 'pe-201984-master':
-  ensure       => 'present',
-  host_aliases => ['pe-201984-master.puppetdebug.vlan'],
-  ip           => '10.234.2.87',
-  target       => '/etc/hosts',
+    ensure       => 'present',
+    host_aliases => ['pe-201984-master.puppetdebug.vlan'],
+    ip           => '10.234.2.87',
+    target       => '/etc/hosts',
+    subscribe    => File['/etc/hosts'],
   }
 
   host { 'centos7-test3':
-  ensure       => 'present',
-  host_aliases => ['centos7-test3.platform9.puppet.net'],
-  ip           => '10.234.3.57',
-  target       => '/etc/hosts',
+    ensure       => 'present',
+    host_aliases => ['centos7-test3.platform9.puppet.net'],
+    ip           => '10.234.3.57',
+    target       => '/etc/hosts',
+    subscribe    => File['/etc/hosts'],
   }
 
 }
